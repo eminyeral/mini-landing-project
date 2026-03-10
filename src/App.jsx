8c8699b8fb3c8b2019af7d92f204329cd072f9e8
eminyeral <eminyerall@gmail.com>
2026-03-11 01:34:01 +0300

    style: implement css variables and global layout structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.scss'
+import Button from './components/Button/Button'; // 1. Önce içeri alıyoruz
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container"> 
+      <h1>Hoş Geldiniz!</h1>
+      <p>Lütfen giriş yapın veya kayıt olun.</p>
+      
+      <div className="button-group">
+        <Button variant="primary">Kayıt Ol</Button>
+        <Button variant="outline">Giriş Yap</Button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
